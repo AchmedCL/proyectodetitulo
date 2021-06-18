83e8544fe74d123b34511cc5fdc871c2dbb607ff
Javier <javier.maul@gmail.com>
2021-06-18 12:38:54 -0400

    asdf

--- src/app/views/inicio-home/inicio-home.component.ts
+++ src/app/views/inicio-home/inicio-home.component.ts
@@ -6,10 +6,6 @@ import { AuthserviceService } from 'src/app/services/auths/authservice.service';
 import { getFormControlOrThrow } from 'src/app/shared/services/form';
 import firebase from 'firebase';
 import * as firebases from 'firebase/app';
-import { User } from 'src/app/shared/services/user';
-import { AngularFireAuth } from '@angular/fire/auth';
-import auth from '../../../../node_modules/firebase'
-
 
 @Component({
   selector: 'app-inicio-home',
@@ -18,42 +14,43 @@ import auth from '../../../../node_modules/firebase'
 })
 export class InicioHomeComponent implements OnInit {
 
-  constructor(private afs: AngularFirestore, private authService: AuthserviceService, private afauth: AngularFireAuth) { 
-   }
+  constructor(private afs: AngularFirestore, private authService: AuthserviceService) { 
+    this.currentUser = authService.getCurrentUser();
+    
+  }
 
   nuevaPublicacion = new FormGroup({
     titulo: new FormControl('', Validators.required),
     cuerpoTexto: new FormControl('',Validators.required),
   });
+  currentUser: Promise<firebase.User | null>;
   publicacion: Publicaciones | undefined;
-  async ngOnInit(): Promise<void> {
-
+  ngOnInit(): void {
+    
   }
 
   async crearNuevaPublicacion(): Promise<boolean>{
     this.publicacion = {
       titulo: this.titulo.value,
       cuerpo: this.texto.value,
-    };
-    console.log(this.publicacion);
-    const user = await this.authService.getCurrentUser();
-    if(!user){
-      return false;
     }
-    const pub = await this.afs.collection('users').doc(user.uid).collection('publicaciones').doc()
+    console.log(this.publicacion);
+    
+    const pub = await this.afs.collection('publicaciones').doc().set(this.publicacion);
     console.log(pub);
     // if(!pub){
     //   return false;
     // }
     
     
-    
+    const user = await this.authService.getCurrentUser();
+    if(!user){
+      return false;
+    }
     //const userUpdated = await this.afs.collection('users').doc(user.uid).update({publicaciones: firebase.firestore.FieldValue.arrayUnion(pub.id)});
     return true;
   }
-  // async getuserCurrent(): Promise<User | null>{
-  //   //return const user = await this.authService.getCurrentUser();
-  // }
+  
   get titulo(): FormControl{
     return getFormControlOrThrow('titulo', this.nuevaPublicacion);
   }
